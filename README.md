31b746f640d2bd3a365d7187f7c42bbba5f1f2d9
Loic4planet <loic.quattrociocchi@4planet.eu>
2017-04-14 01:41:28 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# Space Engineers : Space Solar Rotor
+
+## Description :
+This script auto change velocity (and angle) in rotor to set >80% of Solar panel output.
+
+This script contain a debug result (you can see in screenshots).
+
+## How to use ?
+
+- First, you need to set a TagScript in solar panel and rotor : ```[SSR]```
+
+- Second, set a same tag name (it's your choice) for the solar panel and the linked rotor.
+You need to use a semicolon ```;``` to set a tag name.
+Example name rotor: ```[SSR] Rotor 1 ;downPanel;```
+and name solar panel: ```[SSR] Solar Panel 1 ;downPanel;```
+
+## Debug ?
+
+- You need to see a information, you can use a lcd screen to display informations.
+You need to add the TagScript and the tag debug in lcd.
+Example name lcd: ```[SSR] LCD 1 ;debug;```
+
+## LICENSE
+
+[APACHE LICENSE VERSION 2.0](LICENSE)
---
+++ Script.cs
@@ -0,0 +1,72 @@
+static string tagMod = "[SSR]"; 
+static string tagDebug = "DEBUG"; 
+static float maxCollectedSolarPanel = 120;  
+Dictionary<string,string> dataModule = new Dictionary<string,string> {};  
+  
+/**   
+ * Apache License 2.0   
+ * Written by lquattro (Scuriva Elquattro)  
+ */   
+public void Main(string argument) {  
+  
+    /**   
+     * Variables texts  
+     */   
+    string textDebug = "   ---== Debug ==---";  
+  
+    // Solar Panels  
+    List<IMySolarPanel> solarPanels = new List<IMySolarPanel>();     
+    GridTerminalSystem.GetBlocksOfType<IMySolarPanel>(solarPanels);  
+    // Rotor  
+    List<IMyMotorStator> rotors = new List<IMyMotorStator>();      
+    GridTerminalSystem.GetBlocksOfType<IMyMotorStator>(rotors);  
+  
+    // Text Debug  
+    List<IMyTextPanel> lcds = new List<IMyTextPanel>();       
+    GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(lcds);  
+ 
+    // Set Dictorary Map solar :  Module to Text 
+    Dictionary<string,IMySolarPanel> solars = new Dictionary<string,IMySolarPanel> {}; 
+    foreach ( IMySolarPanel solar in solarPanels) { 
+        string solarName = solar.CustomName; 
+        if ( !solarName.Contains(tagMod) ) continue; 
+
+        string solarNameTag = solarName.Split(';')[1].ToUpper(); 
+        solars[solarNameTag] = solar;
+    } 
+    
+    foreach ( IMyMotorStator rotor in rotors ) { 
+        string rotorName = rotor.CustomName;
+        if ( !rotorName.Contains(tagMod) ) continue; 
+ 
+        IMySolarPanel solar; 
+        string solarNameTag = rotorName.Split(';')[1].ToUpper(); 
+        if ( !solars.TryGetValue( solarNameTag, out solar ) ) continue; 
+ 
+        // Test Solar panel power 
+        float percentage = solar.MaxOutput * 1000 * 100 / maxCollectedSolarPanel; 
+        textDebug += "\n Power kW: " + solar.MaxOutput * 1000; 
+        textDebug += "\n Power %: " + percentage; 
+        if ( percentage >= 80 ) { 
+            rotor.TargetVelocity = (float) 0; 
+            continue; 
+        }
+
+        // Change Velocity 
+        rotor.TargetVelocity = (float) -0.10; 
+        textDebug += "\n Angle: " + rotor.Angle; 
+        textDebug += "\n Displacement: " + rotor.Displacement; 
+        textDebug += "\n Torque: " + rotor.Torque; 
+        textDebug += "\n TargetVelocity: " + rotor.TargetVelocity; 
+    }
+  
+    // Display Text Debug  
+    foreach ( IMyTextPanel lcd in lcds ) { 
+        if ( !lcd.CustomName.Contains(tagMod) && !lcd.CustomName.Contains(tagDebug) ) continue; 
+ 
+        // Write test in displays  
+        lcd.WritePublicText(textDebug, false);  
+        // Show public text in displays   
+        lcd.ShowPublicTextOnScreen();  
+    }  
+}   
